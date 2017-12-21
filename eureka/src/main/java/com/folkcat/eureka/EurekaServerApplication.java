2f83a85147a71896b3631259b13a02eacf8db023
Tamas <donghaifeng@hotmail.com>
2017-12-21 17:10:54 +0800

    First commit 实现Eureka客户端和服务端

---
+++ eureka/src/main/java/com/folkcat/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.folkcat.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Tamas on 2017/12/21.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+
+}
