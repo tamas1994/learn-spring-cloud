c2f9435fbb4e45243800478e979463f89c07cba1
Tamas <donghaifeng@hotmail.com>
2017-12-22 10:52:58 +0800

    添加Zuul Demo 实现网关路由和过滤

---
+++ zuul/src/main/java/com/folkcat/zuul/ServiceZuulApplication.java
@@ -0,0 +1,19 @@
+package com.folkcat.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by Tamas on 2017/12/22.
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+}
