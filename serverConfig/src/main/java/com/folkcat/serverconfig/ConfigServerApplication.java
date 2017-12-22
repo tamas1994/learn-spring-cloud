a8f40f9ae2d238a5cd2855689efac7bff0cf646b
Tamas <donghaifeng@hotmail.com>
2017-12-22 15:08:30 +0800

    实现高可用分布式配置

--- serverConfig/src/main/java/com/folkcat/serverconfig/ConfigServerApplication.java
+++ serverConfig/src/main/java/com/folkcat/serverconfig/ConfigServerApplication.java
@@ -3,12 +3,14 @@ package com.folkcat.serverconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by Tamas on 2017/12/22.
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
---
+++ serverConfigEureka/src/main/java/com/folkcat/serverconfigeureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.folkcat.serverconfigeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Tamas on 2017/12/22.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
