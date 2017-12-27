7158e77125bac9e3579207b831dc57a306d6eed2
Tamas <donghaifeng@hotmail.com>
2017-12-27 16:04:25 +0800

    为feign添加dashboard支持

--- feign/src/main/java/com/folkcat/feign/FeignApplication.java
+++ feign/src/main/java/com/folkcat/feign/FeignApplication.java
@@ -2,8 +2,11 @@ package com.folkcat.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * Created by Tamas on 2017/12/22.
@@ -11,6 +14,9 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class FeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(FeignApplication.class, args);
